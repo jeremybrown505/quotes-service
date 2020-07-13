cb66a08e87d8616cfc23724ffb41891fa3c1c643
Jeremy Brown <Jeremy505brown@gmail.com>
2020-07-13 11:23:49 -0600

    Continuring OAuth 2.0 integration.

--- src/main/java/edu/cnm/deepdive/quotes/service/QuoteRepository.java
+++ src/main/java/edu/cnm/deepdive/quotes/service/QuoteRepository.java
@@ -4,7 +4,9 @@ import edu.cnm.deepdive.quotes.model.entity.Quote;
 import edu.cnm.deepdive.quotes.model.entity.Source;
 import edu.cnm.deepdive.quotes.model.entity.Tag;
 import java.util.Date;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface QuoteRepository extends JpaRepository<Quote, Long> {
 
@@ -18,4 +20,11 @@ public interface QuoteRepository extends JpaRepository<Quote, Long> {
 
   Iterable<Quote> getAllByCreatedBetweenOrderByCreatedAsc(Date start, Date end);
 
+  @SuppressWarnings("SqlResolve")
+  @Query(value = "SELECT * FROM sa.Quote ORDER BY RANDOM() OFFSET 0 ROWS FETCH NEXT 1 ROW ONLY",
+      nativeQuery = true)
+  Optional<Quote> getRandom();
+
+  @Query("SELECT COUNT(q) from Quote As q")
+  long getCount();
 }
